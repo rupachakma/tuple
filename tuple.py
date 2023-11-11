c1ebf836bff6029873401c258fdae7f85f84c493
rupa <rupa@gmil.com>
2023-11-11 23:32:35 +0600

    problem solving

---
+++ tuple.py
@@ -0,0 +1,51 @@
+# thistuple = ("apple", "banana", "cherry")
+# if "mango" in thistuple:
+#     print("yes")
+# else:print("no")
+
+
+# thistuple = ("apple", "banana", "cherry")
+# mylist=list(thistuple)
+# print(type(mylist))
+# mylist.append("kiwi")
+# print(mylist)
+# thistuple=tuple(mylist)
+# print(thistuple)
+# print(type(thistuple))
+
+
+# list=["one","two","three","four","five","six","seven","eight","nine","ten"]
+# x=list.index("ten")
+# print(x)
+
+
+# item=int(input("Enter List Items :"))
+# list=[]
+# for i in range(item):
+#     i=int(input("Enter {} Items :".format(i)))
+#     list.append(i)
+# print(list)
+
+# for i in range(len(list)):
+#     if list[i]=="Ahad":
+#         list.insert[i+1,"Lokman"]
+# print(list)
+
+
+# mytuple=(input("Enter item :"))
+# print(mytuple)
+# list=list(mytuple)
+# print(type(list))
+
+# for i in range(len(list)):
+#     list.insert[1,2]
+# print(list)
+
+
+
+def cube(x):
+    r=x**3
+    return(r)
+
+r=cube(4)
+print(r)
